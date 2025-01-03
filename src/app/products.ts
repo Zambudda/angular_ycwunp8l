db480d26b8a0c37a637831d8379a30c63106a68e
Zambudda <zambluca@gmail.com>
2025-01-03 12:16:02 +0100

    My Commit 02

--- src/app/products.ts
+++ src/app/products.ts
@@ -3,6 +3,7 @@ export interface Product {
   name: string;
   price: number;
   description: string;
+  rating: number;
 }
 
 export const products = [
@@ -10,25 +11,29 @@ export const products = [
     id: 1,
     name: 'Phone XL',
     price: 799,
-    description: 'A large phone with one of the best screens'
+    description: 'A large phone with one of the best screens',
+    rating: 11
   },
   {
     id: 2,
     name: 'Phone Mini',
     price: 699,
-    description: 'A great phone with one of the best cameras'
+    description: 'A great phone with one of the best cameras',
+    rating: 22
   },
   {
     id: 3,
     name: 'Phone Standard',
     price: 299,
-    description: ''
+    description: '',
+    rating: 33
   },
   {
     id: 4,
     name: 'Locomotiva E.646',
     price: 1299,
-    description: 'Locomotova'
+    description: 'Locomotiva',
+    rating: 44
   }
 ];
 
